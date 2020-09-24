10990a1eeef05ac9a2267596d63d6211b93e9627
cube-PC <youngcube@hotmail.com>
2020-09-24 13:39:43 +0800

    windows path

--- src/mainapp.cpp
+++ src/mainapp.cpp
@@ -10,7 +10,13 @@ MainApp::~MainApp(){}
 void MainApp::loadQml()
 {
 #ifdef QT_DEBUG
+    //根据实际项目位置来改
+#ifdef Q_OS_WIN
+    const QUrl url = QUrl::fromLocalFile("D:/SourceTree/qtqmlexample2/resource/qml/main/main.qml");
+#else
     const QUrl url(QStringLiteral("/Users/cube/SourceTree/qtqmlexample2/resource/qml/main/main.qml"));
+#endif
+
 #else
     const QUrl url(QStringLiteral("qrc:/resource/qml/main/main.qml"));
 #endif
